e23c353a8433271c3d8290fc859d34072607dbd2
vinepiaia <47006626+vinepiaia@users.noreply.github.com>
2023-03-09 15:02:17 -0300

    Alterado metodos

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,29 +9,69 @@ namespace calculadora_2._0
 {//problema com numeros negativos
     internal class Program
     {
-        static string expresion = "";
-        static string[] array_expresion;
+        static List<string> array_expresion;
         static string loop = "";
         static void Main(string[] args)
         {
             while (loop != "f")
             {
+                string expresion = "";
                 Console.WriteLine("Welcome to your calculator developed by Leo Technologies\n\n\n\nType your expresion\n");
                 expresion = Console.ReadLine();
                 expresion = expresion.Trim();
                 expresion = expresion.Replace(",", ".");//in case of decimal numbers, protect the user from using , as decimal sign
-                replacing();
-                array_expresion = expresion.Split('@');
-                multiplication();
-                division();
-                subtraction();
-                addition();
+                Replacing();
+                array_expresion = expresion.Split('@').ToList();
+
+
+               
+
+                bool processing = true;
+                while (processing)
+                {
+                    var div = array_expresion.Contains("/");
+                    var mul = array_expresion.Contains("*");
+                    var som = array_expresion.Contains("+");
+                    var sub = array_expresion.Contains("-");
+                    if (!div && !mul && !som && !sub)
+                    {
+                        processing = false;
+                        break;
+                    }
+
+                    if (div == true || mul == true)
+                    {
+                        var idxDiv = array_expresion.IndexOf("");
+                        var idxMul = array_expresion.IndexOf("*");
+                        if (idxDiv < idxMul)
+                        {
+                            var num1 = Convert.ToDouble(array_expresion[idxDiv - 1]);
+                            var num2 = Convert.ToDouble(array_expresion[idxDiv + 1]);
+                            Double result = Division(num1, num2);
+
+                            array_expresion.RemoveRange(Convert.ToInt32(array_expresion[idxDiv - 1]), 3);
+                            array_expresion.Insert(Convert.ToInt32(array_expresion[idxDiv - 1]), result.ToString());
+                        }
+                        else
+                        {
+
+                        }
+
+                    }
+
+                }
+
+
+
+
+
+
                 Console.WriteLine("The result is: " + expresion + "\n\nPress any key to repeat or f to exit");
                 loop = Console.ReadLine().ToLower().Trim();
                 Console.Clear();
             }
         }
-        static void replacing()
+        static void Replacing()
         {
             //replacing negative numbers
             // negative numbers
@@ -61,12 +102,12 @@ namespace calculadora_2._0
                     continue;
                 }
             }
-            for (int i=0;i<expresion.Length;i++)
+            for (int i = 0; i < expresion.Length; i++)
             {
-                
+
                 // replacing operators
                 // +++++++++
-                if (i>0&&expresion.Substring(i, 1) == "+" && expresion.Substring(i - 1, 1) != "@" && expresion.Substring(i + 1, 1) != "@")
+                if (i > 0 && expresion.Substring(i, 1) == "+" && expresion.Substring(i - 1, 1) != "@" && expresion.Substring(i + 1, 1) != "@")
                 {
                     expresion = expresion.Replace("+", "@+@");
                     i = 0;
@@ -94,112 +135,27 @@ namespace calculadora_2._0
                     continue;
                 }
             }
-            if(expresion.Substring(0, 1) == "@")//excecao do replace de -
+            if (expresion.Substring(0, 1) == "@")//excecao do replace de -
             {
                 expresion = expresion.Substring(3);
                 expresion = "-" + expresion;
             }
         }
-        static void multiplication()
+        static double Multiplication(double num1, double num2)
         {
-            /*put the * first*/
-            for (int i = 0; i < array_expresion.Length; i++)
-            {
-                expresion = expresion.Trim();
-                array_expresion = expresion.Split('@');
-                if (array_expresion.Length > 1 && array_expresion[i] == "*")
-                {
-                    expresion = "";
-                    array_expresion[i - 1] = Convert.ToString(Convert.ToDecimal(array_expresion[i - 1]) * Convert.ToDecimal(array_expresion[i + 1]));
-                    array_expresion[i] = "#";
-                    array_expresion[i + 1] = "#";
-                    for (int k = 0; k < array_expresion.Length; k++)
-                    {
-                        if (array_expresion[k] != "#")
-                        {
-                            expresion += array_expresion[k];
-                        }
-                    }
-                    replacing();
-                    i = 0;
-                    continue;
-                }
-            }
+            return num1 * num2;
         }
-        static void division()
+        static double Division(double num1, double num2)
         {
-            for (int i = 0; i < array_expresion.Length; i++)
-            {
-                expresion = expresion.Trim();
-                array_expresion = expresion.Split('@');
-                if (array_expresion.Length > 1 && array_expresion[i] == "/")
-                {
-                    expresion = "";
-                    array_expresion[i - 1] = Convert.ToString(Convert.ToDecimal(array_expresion[i - 1]) / Convert.ToDecimal(array_expresion[i + 1]));
-                    array_expresion[i] = "#";
-                    array_expresion[i + 1] = "#";
-                    for (int k = 0; k < array_expresion.Length; k++)
-                    {
-                        if (array_expresion[k] != "#")
-                        {
-                            expresion += array_expresion[k];
-                        }
-                    }
-                    replacing();
-                    i = 0;
-                    continue;
-                }
-            }
+            return num1 / num2;
         }
-        static void addition()
+        static double Addition(double num1, double num2)
         {
-            for (int i = 0; i < array_expresion.Length; i++)
-            {
-                expresion = expresion.Trim();
-                array_expresion = expresion.Split('@');
-                if (array_expresion.Length > 1 && array_expresion[i] == "+")
-                {
-                    expresion = "";
-                    array_expresion[i - 1] = Convert.ToString(Convert.ToDecimal(array_expresion[i - 1]) + Convert.ToDecimal(array_expresion[i + 1]));
-                    array_expresion[i] = "#";
-                    array_expresion[i + 1] = "#";
-                    for (int k = 0; k < array_expresion.Length; k++)
-                    {
-                        if (array_expresion[k] != "#")
-                        {
-                            expresion += array_expresion[k];
-                        }
-                    }
-                    replacing();
-                    i = 0;
-                    continue;
-                }
-            }
+            return num1 + num2;
         }
-        static void subtraction()
+        static double Subtraction(double num1, double num2)
         {
-            for (int i = 0; i < array_expresion.Length; i++)
-            {
-                expresion = expresion.Trim();
-                array_expresion = expresion.Split('@');
-                if (array_expresion.Length > 1 && array_expresion[i] == "-")
-                {
-                    expresion = "";
-                    array_expresion[i - 1] = Convert.ToString(Convert.ToDecimal(array_expresion[i - 1]) - Convert.ToDecimal(array_expresion[i + 1]));
-                    array_expresion[i] = "#";
-                    array_expresion[i + 1] = "#";
-                    for (int k = 0; k < array_expresion.Length; k++)
-                    {
-                        if (array_expresion[k] != "#")
-                        {
-                            expresion += array_expresion[k];
-                        }
-                    }
-                    replacing();
-                    i = 0;
-                    continue;
-                }
-            }
+            return num1 - num2;
         }
     }
 }
